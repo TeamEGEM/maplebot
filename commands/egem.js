093d3fe3728dbef642fd9e84878e5c1b6e2d15b7
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-11 13:04:36 -0700

    the MCX bot start.

---
+++ commands/egem.js
@@ -0,0 +1,51 @@
+
+const Discord = require("discord.js");
+const miscSettings = require("../cfg/settings.json");
+var getJSON = require('get-json');
+
+exports.run = (client, message, args) => {
+  var btcPrice = getJSON('https://maplechange.com/api/v2/tickers/egembtc.json', function(error, response){
+		if(!error) {
+      var buy = response["ticker"]["buy"];
+      var sell = response["ticker"]["sell"];
+      var low = response["ticker"]["low"];
+      var high = response["ticker"]["high"];
+      var last = response["ticker"]["last"];
+      var vol = response["ticker"]["vol"];
+      var volbtc = response["ticker"]["volbtc"];
+      var change = response["ticker"]["change"];
+      const embed = new Discord.RichEmbed()
+        .setTitle("Maple Change Discord Bot.")
+        .setAuthor("MCX", miscSettings.egemspin)
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor(miscSettings.okcolor)
+        .setDescription(":ledger: Maplechange EGEM Market Data:")
+        .setFooter(miscSettings.footerBranding, miscSettings.img32x32)
+        .setThumbnail(miscSettings.img32shard)
+        /*
+         * Takes a Date object, defaults to current date.
+         */
+        .setTimestamp()
+        .setURL("https://github.com/TeamEGEM/EGEM-Bot")
+        .addField("Buy", buy+" BTC", true)
+        .addField("Sell", sell+" BTC", true)
+        .addField("Low", low+" BTC", true)
+        .addField("High", high+" BTC", true)
+        .addField("Last", last+" BTC", true)
+        .addField("Volume", vol+" EGEM", true)
+        .addField("Volbtc", volbtc+" BTC", true)
+        .addField("Change", change+" %", true)
+        .addField("Quick links:","Direct links to Maplechange.com trade page.")
+        .addField("Bitcoin Pair", "[BTC/EGEM :scales:](https://maplechange.com/markets/egembtc)", true)
+        .addField("Litecoin Pair", "[LTC/EGEM :scales:](https://maplechange.com/markets/egemltc)", true)
+        .addField("Ethereum Pair", "[ETH/EGEM :scales:](https://maplechange.com/markets/egemeth)", true)
+        .addField("WeyCoin Pair", "[WAE/EGEM :scales:](https://maplechange.com/markets/egemwae)", true)
+
+        message.channel.send({embed})
+		} else {
+			console.log('**EGEM BOT** Maplechange MARKET API ISSUE!');
+		}
+	})
+}
---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const fs = require("fs");
+const Discord = require("discord.js");
+const botSettings = require("./botcfg/config.json");
+const miscSettings = require("./cfg/settings.json");
+var getJSON = require('get-json');
+
+
+const prefix = botSettings.prefix;
+const bot = new Discord.Client({disableEveryone:true});
+
+bot.on('ready', ()=>{
+	console.log("**MCX BOT** is now Online.");
+});
+
+// Function to turn files into commands.
+bot.on("message", message => {
+	//if(message.channel.name === '🌐🗣-general' && !message.member.hasPermission('ADMINISTRATOR')) return;
+	//if(message.channel.name != '👾-the-egem-bot') return;
+	if(message.channel.type === "dm") return;
+  if(message.author.bot) return;
+  if(message.content.indexOf(botSettings.prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(bot, message, args);
+  } catch (err) {
+		//console.log("**Maple Change BOT** No file for that command, prolly other system in use.")
+    console.error(err);
+  }
+});
+
+// Login the bot.
+bot.login(botSettings.token);
