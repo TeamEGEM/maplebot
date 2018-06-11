6c5ffccfef0262b972b7a684ce76f5a1b731cef6
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-11 15:39:51 -0700

    MRI added

---
+++ commands/mri.js
@@ -0,0 +1,52 @@
+
+const Discord = require("discord.js");
+const miscSettings = require("../cfg/settings.json");
+var getJSON = require('get-json');
+
+exports.run = (client, message, args) => {
+  var btcPrice = getJSON('https://maplechange.com/api/v2/tickers/mribtc.json', function(error, response){
+		if(!error) {
+      var buy = response["ticker"]["buy"];
+      var sell = response["ticker"]["sell"];
+      var low = response["ticker"]["low"];
+      var high = response["ticker"]["high"];
+      var last = response["ticker"]["last"];
+      var vol = response["ticker"]["vol"];
+      var volbtc = response["ticker"]["volbtc"];
+      var change = response["ticker"]["change"];
+      const embed = new Discord.RichEmbed()
+        .setTitle("Maple Change Discord Bot.")
+        .setAuthor("MCX", miscSettings.img32x32)
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor(miscSettings.msgcolor)
+        .setDescription(":ledger: Maplechange MRI Market Data:")
+        .setFooter(miscSettings.footerBranding, miscSettings.img32x32)
+        .setThumbnail(miscSettings.imgMRI)
+        /*
+         * Takes a Date object, defaults to current date.
+         */
+        .setTimestamp()
+        .setURL("https://github.com/TeamEGEM/maplebot")
+        .addField("Buy", buy+" BTC", true)
+        .addField("Sell", sell+" BTC", true)
+        .addField("Low", low+" BTC", true)
+        .addField("High", high+" BTC", true)
+        .addField("Last", last+" BTC", true)
+        .addField("Volume", vol+" MRI", true)
+        .addField("Volbtc", volbtc+" BTC", true)
+        .addField("Change", change+" %", true)
+        .addField("Quick links:","Direct links to Maplechange.com trade page.")
+        .addField("Bitcoin Pair", "[BTC/MRI :scales:](https://maplechange.com/markets/mribtc)", true)
+        .addField("Litecoin Pair", "[LTC/MRI :scales:](https://maplechange.com/markets/mriltc)", true)
+        .addField("Ethereum Pair", "[ETH/MRI :scales:](https://maplechange.com/markets/mrieth)", true)
+        .addField("WeyCoin Pair", "[ETH/MRI :scales:](https://maplechange.com/markets/mrieth)", true)
+        .addField("Ethereum Pair", "[XGOX/MRI :scales:](https://maplechange.com/markets/xgoxmri)", true)
+
+        message.channel.send({embed})
+		} else {
+			console.log('**MCX BOT** Maplechange MARKET API ISSUE!');
+		}
+	})
+}
