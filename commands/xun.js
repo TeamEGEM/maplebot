d2cd026cd78b62bb50bc6d8d881813f30faec3d4
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-12 06:35:18 -0700

    XLN,XTL,XUN added

---
+++ commands/xun.js
@@ -0,0 +1,51 @@
+
+const Discord = require("discord.js");
+const miscSettings = require("../cfg/settings.json");
+var getJSON = require('get-json');
+
+exports.run = (client, message, args) => {
+  var btcPrice = getJSON('https://maplechange.com/api/v2/tickers/xunbtc.json', function(error, response){
+		if(!error) {
+      var buy = response["ticker"]["buy"];
+      var sell = response["ticker"]["sell"];
+      var low = response["ticker"]["low"];
+      var high = response["ticker"]["high"];
+      var last = response["ticker"]["last"];
+      var vol = response["ticker"]["vol"];
+      var volbtc = response["ticker"]["volbtc"];
+      var change = response["ticker"]["change"];
+      const embed = new Discord.RichEmbed()
+        .setTitle("Maple Change Discord Bot.")
+        .setAuthor("MCX", miscSettings.img32x32)
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor(miscSettings.msgcolor)
+        .setDescription(":ledger: Maplechange XUN Market Data:")
+        .setFooter(miscSettings.footerBranding, miscSettings.img32x32)
+        .setThumbnail(miscSettings.imgXUN)
+        /*
+         * Takes a Date object, defaults to current date.
+         */
+        .setTimestamp()
+        .setURL("https://github.com/TeamEGEM/maplebot")
+        .addField("Buy", buy+" BTC", true)
+        .addField("Sell", sell+" BTC", true)
+        .addField("Low", low+" BTC", true)
+        .addField("High", high+" BTC", true)
+        .addField("Last", last+" BTC", true)
+        .addField("Volume", vol+" XUN", true)
+        .addField("Volbtc", volbtc+" BTC", true)
+        .addField("Change", change+" %", true)
+        .addField("Quick links:","Direct links to Maplechange.com trade page.")
+        .addField("Bitcoin Pair", "[BTC/XUN :scales:](https://maplechange.com/markets/xunbtc)", true)
+        .addField("Litecoin Pair", "[LTC/XUN :scales:](https://maplechange.com/markets/xunltc)", true)
+        .addField("Ethereum Pair", "[LTC/XUN :scales:](https://maplechange.com/markets/xuneth)", true)
+        .addField("WeyCoin Pair", "[WAE/XUN :scales:](https://maplechange.com/markets/xunwae)", true)
+
+        message.channel.send({embed})
+		} else {
+			console.log('**MCX BOT** Maplechange MARKET API ISSUE!');
+		}
+	})
+}
