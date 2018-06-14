ab9a4c490ec04a2a7ed12081f7e250a87acb853d
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-14 10:31:17 -0700

    adding coins

--- func/getprices.js
+++ func/getprices.js
@@ -28,6 +28,42 @@ function getPrice(){
 			console.log('**MCX BOT** WAE API ISSUE!');
 		}
 	})
+
+  var dnrPrice = getJSON('https://maplechange.com/api/v2/tickers/dnrbtc.json', function(error, response){
+		if(!error) {
+			var dnrPrice = response["ticker"]["last"];
+			console.log('**MCX BOT** DNR Price has been logged to the file. ' + dnrPrice +" BTC");
+			fs.writeFile("price/dnr.txt",dnrPrice,(err)=>{
+				if(err) throw err;
+			});
+		} else {
+			console.log('**MCX BOT** DNR API ISSUE!');
+		}
+	})
+
+  var xshPrice = getJSON('https://maplechange.com/api/v2/tickers/xshbtc.json', function(error, response){
+		if(!error) {
+			var xshPrice = response["ticker"]["last"];
+			console.log('**MCX BOT** XSH Price has been logged to the file. ' + xshPrice +" BTC");
+			fs.writeFile("price/xsh.txt",xshPrice,(err)=>{
+				if(err) throw err;
+			});
+		} else {
+			console.log('**MCX BOT** XSH API ISSUE!');
+		}
+	})
+
+  var babyPrice = getJSON('https://maplechange.com/api/v2/tickers/babybtc.json', function(error, response){
+		if(!error) {
+			var babyPrice = response["ticker"]["last"];
+			console.log('**MCX BOT** BABY Price has been logged to the file. ' + babyPrice +" BTC");
+			fs.writeFile("price/baby.txt",babyPrice,(err)=>{
+				if(err) throw err;
+			});
+		} else {
+			console.log('**MCX BOT** BABY API ISSUE!');
+		}
+	})
 }
 
 module.exports = getPrice;
