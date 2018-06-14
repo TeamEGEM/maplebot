cf3e66d502e5969743bfedad55017d251d8dc56f
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-06-14 10:10:52 -0700

    testing

--- func/getprices.js
+++ func/getprices.js
@@ -28,6 +28,18 @@ function getPrice(){
 			console.log('**MCX BOT** EGEM API ISSUE!');
 		}
 	})
+
+  var waePrice = getJSON('https://maplechange.com/api/v2/tickers/waebtc.json', function(error, response){
+		if(!error) {
+			var waePrice = response["ticker"]["last"];
+			console.log('**MCX BOT** WAE Price has been logged to the file. ' + waePrice +" BTC");
+			fs.writeFile("price/wae.txt",waePrice,(err)=>{
+				if(err) throw err;
+			});
+		} else {
+			console.log('**MCX BOT** WAE API ISSUE!');
+		}
+	})
 }
 
 module.exports = getPrice;
