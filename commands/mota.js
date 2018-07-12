7fa2953bbf4cab03a0a4e66061b73c4b2604757a
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-07-12 09:25:39 -0700

    MOTA/GRWI update

--- commands/mota.js
+++ commands/mota.js
@@ -4,7 +4,7 @@ const miscSettings = require("../cfg/settings.json");
 var getJSON = require('get-json');
 
 exports.run = (client, message, args) => {
-  var btcPrice = getJSON('https://maplechange.com/api/v2/tickers/grwibtc.json', function(error, response){
+  var btcPrice = getJSON('https://maplechange.com/api/v2/tickers/motabtc.json', function(error, response){
 		if(!error) {
       var buy = response["ticker"]["buy"];
       var sell = response["ticker"]["sell"];
@@ -23,23 +23,23 @@ exports.run = (client, message, args) => {
         .setColor(miscSettings.msgcolor)
         .setDescription(":ledger: Maple Change GRWI Market Data:")
         .setFooter(miscSettings.footerBranding, miscSettings.img32x32)
-        .setThumbnail(miscSettings.imgGRWI)
+        .setThumbnail(miscSettings.imgMOTA)
         /*
          * Takes a Date object, defaults to current date.
          */
         .setTimestamp()
         .setURL("https://github.com/TeamEGEM/maplebot")
-        .addField("Website:", "https://grwi.io/")
+        .addField("Website:", "https://www.realmotacoin.com/")
         .addField("Buy", buy+" BTC", true)
         .addField("Sell", sell+" BTC", true)
         .addField("Low", low+" BTC", true)
         .addField("High", high+" BTC", true)
         .addField("Last", last+" BTC", true)
-        .addField("Volume", vol+" GRWI", true)
+        .addField("Volume", vol+" MOTA", true)
         .addField("Volbtc", volbtc+" BTC", true)
         .addField("Change", change+" %", true)
         .addField("Quick links:","Direct links to Maplechange.com trade page.")
-        .addField("Bitcoin Pair", "[BTC/GRWI :scales:](https://maplechange.com/markets/grwibtc)", true)
+        .addField("Bitcoin Pair", "[BTC/MOTA :scales:](https://maplechange.com/markets/motabtc)", true)
 
         message.channel.send({embed})
 		} else {
